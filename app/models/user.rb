1b270ea6e9898228c49972bca0633f565c76bfd6
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2021-01-27 03:03:22 -0600

    Enable instances of user to generate their own jwt token.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,10 @@ class User < ApplicationRecord
   validates :username, length: { minimum: 3,
                                  maximum: 10, }
   validates :username, uniqueness: { case_sensitive: true }
+
+  def generate_token
+    JWT.encode({ id: id,
+                 exp: 2.days.from_now.to_i },
+               Rails.application.secrets.secret_key_base)
+  end
 end
