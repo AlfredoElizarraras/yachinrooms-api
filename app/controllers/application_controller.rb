c395e06ee4bbe7f5aa29e662aad711127d6ecd5c
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2021-01-27 03:57:37 -0600

    Add response helper.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
   include ActionController::RequestForgeryProtection
+  include Response
 
   protect_from_forgery with: :exception
   respond_to :json
