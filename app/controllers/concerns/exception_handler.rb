ad1996e226fbe2ea22e925b825d9dd17bcc8b16a
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2021-02-05 02:42:08 -0600

    Add more exceptions to exception handler concern.

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -13,5 +13,17 @@ module ExceptionHandler
     rescue_from JWT::DecodeError do |e|
       json_response({ message: e.message }, :bad_request)
     end
+
+    rescue_from ActiveRecord::RecordNotFound do |e|
+      json_response({ message: e.message }, :not_found)
+    end
+
+    rescue_from ActiveRecord::RecordInvalid do |e|
+      json_response({ message: e.message }, :unprocessable_entity)
+    end
+
+    rescue_from ActionController::ParameterMissing do |e|
+      json_response({ message: e.message }, :bad_request)
+    end
   end
 end
