bc868346dce24080a86f7e6e3a58a4d264231f22
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2021-02-05 02:43:03 -0600

    Use kaminari gem to create room class method to paginate rooms data.

--- app/models/room.rb
+++ app/models/room.rb
@@ -6,4 +6,11 @@ class Room < ApplicationRecord
   validates_presence_of :fee
   validates_presence_of :latitude
   validates_presence_of :longitude
+
+  def self.paginate_with_users(page = 1, per_page = 20)
+    rooms = Room.order(created_at: :desc)
+      .eager_load(:host).to_a
+    paginated_rooms = Kaminari.paginate_array(rooms)
+    paginated_rooms.page(page).per(per_page)
+  end
 end
