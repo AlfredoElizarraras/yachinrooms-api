b5782ec93b6d528eddb39a3896bdc65ade3b603a
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2021-02-05 02:20:57 -0600

    Add a before action to convert incoming parameters in camelCase to snake_case.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,16 @@ class ApplicationController < ActionController::API
 
   protect_from_forgery with: :exception
   respond_to :json
+  before_action :snake_case_params
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user
   skip_before_action :verify_authenticity_token
 
   private
+
+  def snake_case_params
+    request.parameters.deep_transform_keys!(&:underscore)
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
